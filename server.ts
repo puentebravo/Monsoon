fe506a184f72c4223cfdbf6ced8dcadbd9f51d40
Charlie Puente <puentematos@gmail.com>
2022-07-22 20:04:05 -0400

    initial commit'

---
+++ server.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+import {Request, Response } from "express"
+
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("./client/public"));
+}
+
+app.get("*", function (req: Request, res: Response) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server now listening on ${PORT}. Check localhost:`, PORT);
+});
