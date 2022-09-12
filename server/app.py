98d901b7c4ad3c05b7bd15aba76476be83ab2225
Puentebravo <puentematos@gmail.com>
2022-09-11 20:43:12 -0400

    Replaced node backend with python/Flask

---
+++ server/app.py
@@ -0,0 +1,26 @@
+
+from flask import Flask
+
+def create_app(text_config=None):
+    app = Flask(__name__)
+    app.url_map.strict_slashes= False
+
+    @app.route("/getWeather")
+    def getWeather():
+        response_body: dict = {
+            "name": "Weather",
+            "Temp": "It is 76F and sunny today"
+        }
+
+        return response_body
+
+    @app.route("/")
+    def home():
+        return "<h1> Under development </h1>"
+
+    return app
+
+
+
+
+
