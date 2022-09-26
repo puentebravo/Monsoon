d3a3ea5e6ba246e8e8f196644cb7b745284e16bf
Puentebravo <puentematos@gmail.com>
2022-09-25 22:32:33 -0400

    Changed component name, scaffolded base layout

---
+++ client/src/components/botSelector.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function BotSelector() {
+    return (
+        <section className="container bg-slate-400/20 px-4 max-w-xs ml-4 mt-40">
+            <ul className="list-none">
+                <li className="text-white">Testing </li>
+                <li className="text-white">out</li>
+                <li className="text-white">this</li>
+                <li className="text-white">container</li>
+            </ul>
+        </section>
+    )
+}
+
+export default BotSelector;
